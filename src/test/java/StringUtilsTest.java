af54e6ae33b81927e636b37a5f8fb3624e533d40
Jeremy FROMINVILLE <j.e.fromin@gmail.com>
2019-11-27 17:03:11 +0100

    TP - Junit Exercice 1

---
+++ src/test/java/StringUtilsTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dev.utils.StringUtils;
+
+public class StringUtilsTest {
+	
+	@Test
+	public void levenshteinDistanceTest(){
+		String chaines1[]= {"chat","machins","aviron","distance","Chien",""};
+		String chaines2[]= {"chats","machine","avion","instance","Chine","chat"};
+		int resultatsAttendus[]= {1,1,1,2,2,4};
+		int resultatsTests[]= new int[chaines1.length];
+		
+		for(int i= 0; i< chaines1.length; i++){
+			resultatsTests[i]= StringUtils.levenshteinDistance(chaines1[i], chaines2[i]);
+			System.out.println(resultatsTests[i]);
+		}
+		
+		assertArrayEquals(resultatsAttendus, resultatsTests);
+	}
+	
+	@Test
+	public void levenshteinDistanceTestAvecNull(){
+		String chaines1[]= {null,null,"aviron"};
+		String chaines2[]= {null,"machine",null};
+		int resultatsAttendus[]= {0,7,6};
+		int resultatsTests[]= new int[chaines1.length];
+		
+		for(int i= 0; i< chaines1.length; i++){
+			resultatsTests[i]= StringUtils.levenshteinDistance(chaines1[i], chaines2[i]);
+			System.out.println(resultatsTests[i]);
+		}
+		
+		assertArrayEquals(resultatsAttendus, resultatsTests);
+	}
+	
+}
